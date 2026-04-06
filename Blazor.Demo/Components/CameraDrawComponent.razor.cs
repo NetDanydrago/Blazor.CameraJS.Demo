c4914d3a271ab822146ab49ecd713b5420bd5b1c
Daniel Rosas Mialma <dan11_daniel@hotmail.com>
2026-04-06 16:57:11 -0600

    Add project files.

---
+++ Blazor.Demo/Components/CameraDrawComponent.razor.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Blazor.Demo.Components;
+
+public partial class CameraDrawComponent : IAsyncDisposable
+{
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+
+    private ElementReference VideoReference { get; set; }
+
+    private ElementReference CanvasReference { get; set; }
+
+    private TaskCompletionSource<IJSObjectReference> ModuleReferenceSource { get; set; }
+    private TaskCompletionSource<IJSObjectReference> CameraDrawReferenceSource { get; set; }
+
+    private bool IsCanvasInitialized { get; set; }
+    private bool IsCameraActive { get; set; }
+
+    public CameraDrawComponent()
+    {
+        ModuleReferenceSource = new();
+        CameraDrawReferenceSource = new();
+    }
+
+
+    protected async override Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            var moduleReference = await JSRuntime.InvokeAsync<IJSObjectReference>("import",
+                                                       "./Components/CameraDrawComponent.razor.js");
+            var cameraDrawReference = await moduleReference.InvokeConstructorAsync("CameraDrawComponent", 
+                                          VideoReference, CanvasReference);
+
+            ModuleReferenceSource.SetResult(moduleReference);
+            CameraDrawReferenceSource.SetResult(cameraDrawReference);
+            IsCanvasInitialized = true;
+        }
+    }
+
+    private async Task StartCameraAsync()
+    {
+        if (!IsCameraActive)
+        {
+            IJSObjectReference cameraDrawReference = await CameraDrawReferenceSource.Task;
+            await cameraDrawReference.InvokeVoidAsync("startCamera");
+            IsCameraActive = true;
+        }
+    }
+
+    private async Task TakePhotoAsync()
+    {
+        if (IsCameraActive && IsCanvasInitialized)
+        {
+            IJSObjectReference cameraDrawReference = await CameraDrawReferenceSource.Task;
+            await cameraDrawReference.InvokeVoidAsync("takePhoto");
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (IsCanvasInitialized)
+        {
+            IJSObjectReference cameraDrawReference = await CameraDrawReferenceSource.Task;
+            IJSObjectReference moduleReference = await ModuleReferenceSource.Task;
+
+            await cameraDrawReference.InvokeVoidAsync("dispose");
+            await cameraDrawReference.DisposeAsync();
+            await moduleReference.DisposeAsync();
+
+            IsCameraActive = false;
+        }
+    }
+}
---
+++ Blazor.Demo/Components/CameraDrawComponent.razor.js
@@ -0,0 +1,35 @@
+﻿export class CameraDrawComponent {
+
+    constructor(video, canvas) {
+        this.video = video;
+        this.canvas = canvas;
+        this.context = canvas.getContext("2d");
+    }
+
+    async startCamera() {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+        this.video.srcObject = stream;
+        await this.video.play();
+    }
+
+    stopCamera() {
+        const stream = this.video.srcObject;
+
+        if (stream && stream.getTracks) {
+            const tracks = stream.getTracks();
+            tracks.forEach(track => track.stop());
+        }
+
+        this.video.srcObject = null;
+    }
+
+    takePhoto() {
+        this.context.globalCompositeOperation = "source-over";
+        this.context.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
+    }
+
+
+    dispose() {
+        this.stopCamera();
+    }
+}
